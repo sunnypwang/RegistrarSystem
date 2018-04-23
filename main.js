49ba5ed521a3d7f5571013d37b8c4a4b9eb8bf04
Sarot Busala <sabuza97@gmail.com>
2018-04-24 03:48:52 +0700

    send JSON instead of webpage

--- main.js
+++ main.js
@@ -29,6 +29,47 @@ app.use(function (req, res, next) {
     next();
 });
 
+//SESSION
+app.use(session({
+    key: 'user_sid',
+    secret: 'nani koreeeeee',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+  }));
+
+/*
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
+});
+*/
+
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+    //if (req.session.user && req.cookies.user_sid) {
+    if (req.session.user){
+        res.redirect('/');
+    } else {
+        next();
+    }    
+};
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
 http.listen(3000, function(){
     console.log('started on port 3000');
 });
@@ -40,7 +81,57 @@ app.get('/', function(req, res){
 });
 
 app.post('/main', function(req, res){
+    console.log(req.body.usertype,req.body.id,req.body.password);
+    //these should be received from "HTML form" object
+    var type = req.body.usertype;
+    var result;
+    if(type == "Student"){
+        console.log(req.body.id);
+        db.query('SELECT * FROM student s WHERE s.StudentID = ?', [req.body.id], (err,rows) => {
+            if(err) throw err;
+            
+            if(rows[0] != undefined){
+                if(rows[0].password == req.body.password){
+                    //res.sendFile('index.html', { root: __dirname });
+                    //res.setHeader('Content-Type', 'application/json');
+                    //res.send(JSON.stringify ({ userid : req.body.id}));
+                    res.json({username : "walawwwwwwwwwwwww"});
+                    loggedin = true;
+                    userid = req.body.id;
+                }else{
+                    console.log("password incorrect");
+                    res.sendFile('wrong.html', { root: __dirname });
+                }
+            }else{
+                console.log("username incorrect");
+                res.sendFile('wrong.html', { root: __dirname });
+            }   
+            
+        });
+    }else{
+        db.query('SELECT * FROM instructor i WHERE i.InstID = ?', [req.body.id], (err,rows) => {
+            if(err) throw err;
 
+            if(rows[0] != undefined){
+                if(rows[0].password == req.body.password){
+                    res.sendFile('index.html', { root: __dirname });
+                    loggedin = true;
+                    userid = req.body.id;
+                }else{
+                    console.log("password incorrect");
+                    res.sendFile('wrong.html', { root: __dirname });
+                }
+            }else{
+                console.log("username incorrect");
+                res.sendFile('wrong.html', { root: __dirname });
+            }   
+        });
+    }
+    
+});
+/*
+app.post('/main', function(req, res){
+    console.log(req.body.usertype,req.body.id,req.body.password);
     //these should be received from "HTML form" object
     var type = req.body.usertype;
     var result;
@@ -85,7 +176,7 @@ app.post('/main', function(req, res){
     }
     
 });
-
+*/
 
 //example query
 app.get("/courseinfo", function(req, res) {
